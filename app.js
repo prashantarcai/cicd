4ac34ab7e9a0ed0302ee0db6e2554c4b6886d2a0
root <root@cicd.us-central1-f.c.testing-101-394516.internal>
2025-01-05 12:20:03 +0000

    Initial commit for test Node.js app

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World! Your Node.js app is running on GCP VM!');
+});
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
