bc3dddbd406463910188c89c70a30e99030264cb
prashantarcai <prashant@arc.ai>
2025-01-06 13:29:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require('express');  // Import express
-const app = express();               // Create an express app
-const port = process.env.PORT || 8080; // Use the PORT environment variable provided by Cloud Run, or default to 8080
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080; // Use the port provided by Cloud Run or default to 8080
 
-// Define a simple route
 app.get('/', (req, res) => {
-  res.send('Hello, World! Your Node.js app is running on Cloud Run!');
+  res.send('Hello from Cloud Run!');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
