ee47ad67840211c087acc38c39b998528a535e6a
prashantarcai <prashant@arc.ai>
2025-01-06 11:40:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080; // Use the PORT environment variable
 
 app.get('/', (req, res) => {
-  res.send('Successfull, World! Your Node.js app is running on GCP vm');
+  res.send('Hello, World! Your Node.js app is running on Cloud Run!');
 });
 
 app.listen(port, () => {
-  console.log(`App running at http://localhost:${port}`);
+  console.log(`App listening on port ${port}`);
 });
